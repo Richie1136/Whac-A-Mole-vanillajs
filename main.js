b9ca994bc0c96059e55b4549ff3f3c6d3deff3cc
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-05-13 13:35:07 -0400

    Removing the mole class from any class that may have the mole class and then im getting a random position

--- main.js
+++ main.js
@@ -2,8 +2,15 @@ let squares = document.querySelectorAll('.square')
 let mole = document.querySelector('.mole')
 let score = document.querySelector('#score')
 let timeLeft = document.querySelector('#time-left')
-console.log(squares)
 
-squares.forEach((square) => {
-  square.addEventListener('click')
-})
+let result = 0
+
+const randomSquare = () => {
+  squares.forEach((square) => {
+    square.classList.remove('mole')
+  })
+  let randomPos = squares[Math.floor(Math.random() * squares.length)]
+  console.log(randomPos)
+}
+
+randomSquare()
